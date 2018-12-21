a45cdb15ae067cf62a27947299f9eefd77e89e32
saiyavong mitthasone <saiyavongmitthasone@gmail.com>
2018-12-21 23:43:03 +0700

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.static('./dist/program-coder'))
-app.get('/*', (req,res)=>{
+app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '/dist/program-coder/indext.html'))
 })
 
